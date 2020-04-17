21b2b0ce9ed498521e4e3402f7fdfc2d5ad4a0f5
Ilkhom Nazhmudinov <inajmudinov@yahoo.com>
2020-04-17 00:18:10 -0700

    Исправил код для ревью

--- 344 Reverse String-1.py
+++ 344 Reverse String-1.py
@@ -1,4 +1,11 @@
-a = input().split(",")
-
-print(a[::-1])
+def reverse_string(s):
+    chars = list(s)
+    for i in range(len(s) // 2):
+        tmp = chars[i]
+        chars[i] = chars[len(s) - i - 1]
+        chars[len(s) - i - 1] = tmp
+    return ''.join(chars)
+ 
+data = reverse_string('hello')
+print(data) # 'olleh'
 
