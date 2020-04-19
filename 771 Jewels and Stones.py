fdbc72db81fd32cc7f88c1445db8427809ec4c5e
Ilkhom Nazhmudinov <inajmudinov@yahoo.com>
2020-04-18 22:26:30 -0700

    Задачка успешно прошла все тесты.

--- 771 Jewels and Stones.py
+++ 771 Jewels and Stones.py
@@ -1,15 +1,32 @@
-"""J = "aA" 
-S = "aAAbbbb"
-#Output: 3
-"""
-J = "z" 
-S = "ZZ"
-#Output: 0
+import sys
 
-count=0
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count=0
+        for i in range (len(J)):
+            for j in range (len(S)):
+                if J[i]==S[j]:
+                    count += 1
+        return count
+                  
 
-for i in range (len(J)):
-    for j in range (len(S)):
-        if J[i]==S[j]:
-            count += 1
-print(count)
+    # здесь твой код
+
+# здесь напишем все тесты
+def run_tests():
+    
+    
+  # создаём объект экземпляр Solution (совсем простенькое ООП)
+    sol = Solution()
+    
+  # печатаем тесты, вызываю метод этого объекта
+    print(sol.numJewelsInStones("aA", "aAAbbbb"))
+    print(sol.numJewelsInStones("aA", "bb"))
+
+# вот это запустится только если будешь запускать скрипт из консоли
+# $ python 771 Jewels and Stones.py
+# а когда этот код будет запускать LeetCode, то выполняться не будет, что нам и нужно
+if sys.stdin.isatty():
+    run_tests()
+
+        
