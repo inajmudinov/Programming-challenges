6551bab8d5e920679e4b1636481d1647fe0098f5
Ilkhom Nazhmudinov <inajmudinov@yahoo.com>
2020-04-21 23:59:46 -0700

    Изменил код на более быстрый вариани.

--- 771 Jewels and Stones.py
+++ 771 Jewels and Stones.py
@@ -3,10 +3,10 @@ import sys
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         count=0
-        for i in range (len(J)):
-            for j in range (len(S)):
-                if J[i]==S[j]:
-                    count += 1
+        cache=set(J)
+        for ch in S:
+            if ch in cache:
+                count += 1
         return count
                   
 
