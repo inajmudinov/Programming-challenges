1032dd30a6e195fe13cd6d2f432c8358abe3372a
Ilkhom Nazhmudinov <inajmudinov@yahoo.com>
2020-04-25 00:27:40 -0700

    Загнал под шаблон LC

--- 709 To Lower Case leetcode.py
+++ 709 To Lower Case leetcode.py
@@ -1,31 +1,12 @@
-#word = "Hello"
+# Под шаблон LC
 
-b ={"A":"a", "B":"b", "C":"c", "D":"d", "E":"e", "F":"f", "G":"g", "H":"h", "I":"i", "J":"j", "K":"k", "L":"l", "M":"m", "N":"n", "O":"o", "P":"p", "Q":"q", "R":"r", "S":"s", "T":"t", "U":"u", "V":"v", "W":"w", "X":"x", "Y":"y", "Z":"z" }
-
-
-
-def ToLowerCase(word):
-    c = ""
-    for i in word:
-         if i not in b.keys():
-             c += i
-         else:
-             c += b[i]
-    print(c)
-         
-ToLowerCase("Hello")
-
-
-
-#        i == b[i]
-        
-        
-#print(b.keys())
-
-
-
-
-
-
-
-#print(ord("a") - ord("A"))
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        b ={"A":"a", "B":"b", "C":"c", "D":"d", "E":"e", "F":"f", "G":"g", "H":"h", "I":"i", "J":"j", "K":"k", "L":"l", "M":"m", "N":"n", "O":"o", "P":"p", "Q":"q", "R":"r", "S":"s", "T":"t", "U":"u", "V":"v", "W":"w", "X":"x", "Y":"y", "Z":"z"}
+        c = ""
+        for i in str:
+            if i not in b.keys():
+                c += i
+            else:
+                c += b[i]
+        return c
